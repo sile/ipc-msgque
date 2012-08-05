33c2e65f89ff8e2a8886a16d671ca3c9fe1c03e4
Takeru Ohta <phjgt308@gmail.com>
2012-08-05 14:18:47 +0900

    固定長アロケータ用の動作確認コマンドファイル追加

--- Makefile
+++ Makefile
@@ -1,8 +1,12 @@
-all: msgque-test allocator-test mt-malloc-test mt-alloc-test
+all: msgque-test block-allocator-test allocator-test mt-malloc-test mt-alloc-test
 
 allocator-test:
 	mkdir -p bin
 	g++ -Iinclude -O2 -o bin/allocator-test src/bin/allocator-test.cc
+
+block-allocator-test:
+	mkdir -p bin
+	g++ -Iinclude -O2 -o bin/block-allocator-test src/bin/block-allocator-test.cc
 
 msgque-test:
 	mkdir -p bin
---
+++ src/bin/block-allocator-test.cc
@@ -0,0 +1,75 @@
+#include <iostream>
+#include <string.h>
+#include <imque/block_allocator.hh>
+#include <imque/shared_memory.hh>
+
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <unistd.h>
+#include <stdlib.h>
+
+typedef imque::Allocator allocator;
+
+const int CHILD_NUM = 500;
+const int LOOP_COUNT = 1000;
+
+void sigsegv_handler(int sig) {
+  std::cerr << "#" << getpid() << ":" << sig << std::endl;
+  exit(1);
+}
+
+void child_start(allocator& alc) {
+  std::cout << "# child: " << getpid() << std::endl;
+  srand(time(NULL));
+
+  for(int i=0; i < LOOP_COUNT; i++) {
+    unsigned size = (rand() % 1024) + 1;
+
+    uint32_t idx = alc.allocate(size);
+    //std::cout << "[" << getpid() << "] " << size << " => " << idx << std::endl;
+    memset(alc.ptr<char>(idx), rand()%0x100, size);
+    usleep(rand() % 400); 
+    assert(alc.release(idx));
+
+    /*
+    char *buf = new char[size];
+    memset(buf, rand()%0x100, size);
+    usleep(rand() % 400);
+    delete [] buf;
+    */
+  }
+  std::cout << "# exit: " << getpid() << std::endl;
+}
+
+
+int main() {
+  pid_t children[CHILD_NUM];
+
+  imque::SharedMemory mm(1024*CHILD_NUM);
+  if(! mm) {
+    std::cerr << "mmap() failed" << std::endl;
+    return 1;
+  }
+  allocator alc(mm.ptr<void>(), mm.size());
+  alc.init();
+  signal(SIGSEGV, sigsegv_handler);
+
+  for(int i=0; i < CHILD_NUM; i++) {
+    pid_t child = fork();
+    if(child == 0) {
+      child_start(alc);
+      return 0;
+    }
+    children[i] = child;
+  }
+
+  //child_start(alc);
+
+  for(int i=0; i < CHILD_NUM; i++) {
+    waitpid(children[i], NULL, 0);
+  }
+
+  std::cout << "leaked node count: " << alc.allocatedNodeCount() << std::endl;
+
+  return 0;
+}
