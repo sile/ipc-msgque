3a5991197667e202e4b504588e04c2f7e201828a
Takeru Ohta <phjgt308@gmail.com>
2012-08-07 02:25:46 +0900

    665fadcにrevert allocate呼び出し回数を増やさずにABA問題に完全に対処するのが困難そうなので

--- include/imque/queue.hh
+++ include/imque/queue.hh
@@ -10,14 +10,14 @@
 namespace imque {
   class Queue {
   public:
-    Queue(size_t data_size)
-      : shm_(QueueImpl::calc_need_byte_size(data_size)),
-        impl_(shm_) {
+    Queue(size_t entry_count, size_t data_size)
+      : shm_(QueueImpl::calc_need_byte_size(entry_count, data_size)),
+        impl_(entry_count, shm_) {
     }
       
-    Queue(size_t data_size, const std::string& filepath, mode_t mode=0660)
-      : shm_(filepath, QueueImpl::calc_need_byte_size(data_size), mode),
-        impl_(shm_) {
+    Queue(size_t entry_count, size_t data_size, const std::string& filepath, mode_t mode=0660)
+      : shm_(filepath, QueueImpl::calc_need_byte_size(entry_count, data_size), mode),
+        impl_(entry_count, shm_) {
     }
 
     operator bool() const { return shm_ && impl_; }
@@ -32,7 +32,8 @@ namespace imque {
 
     bool deq(std::string& data) { return impl_.deq(data); }
     
-    bool isEmpty() { return impl_.isEmpty(); }
+    bool isEmpty() const { return impl_.isEmpty(); }
+    bool isFull()  const { return impl_.isFull(); }
 
     size_t overflowedCount() const { return impl_.overflowedCount(); }
     void resetOverflowedCount() { impl_.resetOverflowedCount(); }
--- include/imque/queue_impl.hh
+++ include/imque/queue_impl.hh
@@ -11,143 +11,160 @@
 namespace imque {
   class QueueImpl {
     struct Entry {
-      uint32_t next;
-      uint32_t size;
-      char data[0];
+      uint32_t state:1;
+      uint32_t value:31;
 
-      static const uint32_t END = 0;
+      enum STATE {
+        FREE = 0,
+        USED = 1
+      };
+
+      uint32_t* uint32_ptr() { return reinterpret_cast<uint32_t*>(this); }
+      uint32_t  uint32() const { return *reinterpret_cast<const uint32_t*>(this); }
     };
 
     struct Stat {
       uint32_t overflowed_count;
     };
 
-    struct Ver {
-      uint32_t ver;
-      uint32_t val;
-      
-      static Ver fromUint64(uint64_t n) { 
-        uint64_t* tmp = &n;
-        return *reinterpret_cast<Ver*>(tmp);
-      }
-
-      uint64_t toUint64() {
-        return __sync_add_and_fetch(reinterpret_cast<uint64_t*>(this), 0);
-      }
-
-      uint64_t* toUint64Ptr() {
-        return reinterpret_cast<uint64_t*>(this);
-      }
-
-      Ver snapshot() {
-        return Ver::fromUint64(toUint64());
-      }
-    };
-
     struct Header {
-      /*volatile*/ Ver head;
-      /*volatile*/ Ver tail;
-
+      volatile uint32_t read_pos;
+      volatile uint32_t write_pos;
       Stat stat;
+      uint32_t entry_count;
+      Entry entries[0];
+      
+      uint32_t next_read_pos() const { return (read_pos+1)%entry_count; }
+      uint32_t next_write_pos() const { return (write_pos+1)%entry_count; }
     };
 
   public:
-    QueueImpl(SharedMemory& shm)
+    QueueImpl(size_t entry_count, SharedMemory& shm)
       : que_(shm.ptr<Header>()),
-        alc_(shm.ptr<void>(que_size()), shm.size()-que_size()) {
+        alc_(shm.ptr<void>(que_size(entry_count)), shm.size()-que_size(entry_count)) {
+      if(shm) {
+        que_->entry_count = entry_count;
+      }
     }
-    
+
     operator bool() const { return alc_; }
     
     void init() {
       alc_.init();
       
-      uint32_t dummy_head = alc_.allocate(sizeof(Entry));
-      if(dummy_head != 0) {
-        alc_.ptr<Entry>(dummy_head)->next = Entry::END;
-      }
-      que_->head.ver = 0;
-      que_->head.val = dummy_head;
-      que_->tail.ver = 0;
-      que_->tail.val = dummy_head;
-      
+      que_->read_pos  = 0;
+      que_->write_pos = 0;
       que_->stat.overflowed_count = 0;
-    }
-
-    Ver get_tail() {
-      Ver tail = que_->tail.snapshot();
-      Entry* e = alc_.ptr<Entry>(tail.val);
-      uint32_t e_next = e->next;
-      if(e_next == Entry::END) {
-        return tail;
-      }
-      
-      Ver new_tail = {tail.ver+1, e_next};
-      __sync_bool_compare_and_swap(que_->tail.toUint64Ptr(), tail.toUint64(), new_tail.toUint64());
-      return get_tail();
+      memset(que_->entries, 0, sizeof(Entry)*que_->entry_count);
     }
 
     bool enq(const void* data, size_t size) {
-      uint32_t alloc_id = alc_.allocate(sizeof(Entry) + size);
-      if(alloc_id == 0) {
+      if(isFull()) {
         __sync_add_and_fetch(&que_->stat.overflowed_count, 1);
         return false;
       }
       
-      Entry* e = alc_.ptr<Entry>(alloc_id);
-      e->next = Entry::END;
-      e->size = size;
-      memcpy(e->data, data, size);
-
-      for(;;) {
-        Ver tail = get_tail();
-        assert(tail.val != alloc_id);
-        Entry* e2 = alc_.ptr<Entry>(tail.val);
-        if(__sync_bool_compare_and_swap(&e2->next, Entry::END, alloc_id)) {
-          break;
-        }
+      uint32_t alloc_id = alc_.allocate(sizeof(size_t) + size);
+      if(alloc_id == 0) {
+        __sync_add_and_fetch(&que_->stat.overflowed_count, 1);
+        return false;
       }
 
+      alc_.ptr<size_t>(alloc_id)[0] = size;
+      memcpy(alc_.ptr<void>(alloc_id, sizeof(size_t)), data, size);
+
+      if(enq_impl(alloc_id) == false) {
+        __sync_add_and_fetch(&que_->stat.overflowed_count, 1);
+        alc_.release(alloc_id);
+        return false;
+      }
+      
       return true;
     }
 
     bool deq(std::string& buf) {
-      for(;;) {
-        Ver head = que_->head.snapshot();
-        if(isEmpty()) {
-          return false; // empty
-        }
-        assert(head.ver < que_->tail.ver);
-
-        Entry* e = alc_.ptr<Entry>(head.val);
-        uint32_t e_next = e->next;
-        if(que_->head.toUint64() != head.toUint64()) {
-          continue;
-        }
-        assert(e_next != Entry::END);
-        
-        Ver new_head = {head.ver+1, e_next};
-        if(__sync_bool_compare_and_swap(que_->head.toUint64Ptr(), head.toUint64(), new_head.toUint64())) {
-          Entry* e2 = alc_.ptr<Entry>(e_next);
-          buf.assign(e2->data, e2->size);
-          assert(alc_.release(head.val));
-          return true;
-        }
+      if(isEmpty()) {
+        return false;
       }
+
+      uint32_t alloc_id = deq_impl();
+      if(alloc_id == 0) {
+        return false;
+      }
+
+      size_t size = alc_.ptr<size_t>(alloc_id)[0];
+      char*  data = alc_.ptr<char>(alloc_id, sizeof(size_t));
+      buf.assign(data, size);
+      
+      assert(alc_.release(alloc_id));
+      
+      return true;
     }
 
-    bool isEmpty() { return que_->head.ver == get_tail().ver; }
+    bool isEmpty() const { return que_->read_pos == que_->write_pos; }
+    bool isFull()  const { return que_->read_pos == que_->next_write_pos(); }
 
-    static size_t calc_need_byte_size(size_t data_size) {
-      return que_size() + dat_size(data_size);
+    static size_t calc_need_byte_size(size_t entry_count, size_t data_size) {
+      return que_size(entry_count) + dat_size(data_size);
     }
 
     size_t overflowedCount() const { return que_->stat.overflowed_count; }
     void resetOverflowedCount() { que_->stat.overflowed_count = 0; }
 
   private:
-    static size_t que_size() {
-      return sizeof(Header);
+    bool enq_impl(uint32_t value) {
+      uint32_t curr_read  = que_->read_pos;
+      uint32_t curr_write = que_->write_pos;
+      uint32_t next_write = (curr_write+1) % que_->entry_count;
+      
+      if(curr_read == next_write) {
+        __sync_add_and_fetch(&que_->stat.overflowed_count, 1);
+        return false;
+      }
+      
+      Entry* pe = &que_->entries[curr_write];
+      Entry  e = *pe;
+      if(e.state != Entry::FREE) {
+        __sync_bool_compare_and_swap(&que_->write_pos, curr_write, next_write);
+        return enq_impl(value);
+      }
+
+      Entry new_e = {Entry::USED, value};
+      if(__sync_bool_compare_and_swap(pe->uint32_ptr(), e.uint32(), new_e.uint32()) == false) {
+        return enq_impl(value);
+      }
+      
+      __sync_bool_compare_and_swap(&que_->write_pos, curr_write, next_write);      
+      return true;
+    }
+
+    uint32_t deq_impl() {
+      uint32_t curr_read  = que_->read_pos;
+      uint32_t curr_write = que_->write_pos;
+      uint32_t next_read = (curr_read+1) % que_->entry_count;
+      
+      if(curr_read == curr_write) {
+        return 0;
+      }
+
+      Entry* pe = &que_->entries[curr_read];
+      Entry   e = *pe;
+      if(e.state == Entry::FREE) {
+        __sync_bool_compare_and_swap(&que_->read_pos, curr_read, next_read);
+        return deq_impl();
+      }
+
+      Entry new_e = {Entry::FREE, 0};
+      if(__sync_bool_compare_and_swap(pe->uint32_ptr(), e.uint32(), new_e.uint32()) == false) {
+        return deq_impl();
+      }
+      
+      __sync_bool_compare_and_swap(&que_->read_pos, curr_read, next_read);
+      return e.value;
+    }
+
+    static size_t que_size(size_t entry_count) {
+      return sizeof(Header) + sizeof(Entry)*entry_count;
     }
     
     static size_t dat_size(size_t data_size) {
