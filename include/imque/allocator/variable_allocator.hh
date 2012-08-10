8d178d96fd18c76a4b0de0334d6b9365755746f5
Takeru Ohta <phjgt308@gmail.com>
2012-08-10 12:54:29 +0900

    ディレクトリ構成変更(途中)

---
+++ include/imque/allocator/variable_allocator.hh
@@ -0,0 +1,17 @@
+#ifndef IMQUE_ALLOCATOR_VARIABLE_ALLOCATOR_HH
+#define IMQUE_ALLOCATOR_VARIABLE_ALLOCATOR_HH
+
+#include "../atomic/atomic.hh"
+
+namespace imque {
+  namespace allocator {
+    class VariableAllocator {
+    public:
+      
+    private:
+      
+    };
+  }
+}
+
+#endif
