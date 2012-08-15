a103ffc280f3df11988e5540acd471aa1acdd9a0
Takeru Ohta <phjgt308@gmail.com>
2012-08-16 07:53:00 +0900

    アロケーションテストコマンド実装中

--- src/bin/allocator-test.cc
+++ src/bin/allocator-test.cc
@@ -1,23 +1,85 @@
-#include <iostream>
-#include <string.h>
 #include <imque/ipc/shared_memory.hh>
 #include <imque/allocator/variable_allocator.hh>
+
+#include <iostream>
+#include <string>
+#include <vector>
+#include <string.h>
+#include <stdlib.h>
 
 #include <sys/types.h>
 #include <sys/wait.h>
 #include <unistd.h>
-#include <stdlib.h>
+#include <errno.h>
 
 /*
  * パラメータ:
  * - プロセス数:
  * - 各プロセスのスレッド数:
  * - ループ数
- * - 割当サイズの幅
+ * - 割当サイズの幅(min .. max)
  * - interval
  * - アロケート方法(malloc or variable_allocator or block_allocator)
  */
 
+struct Parameter {
+  std::string method; // "variable" | "fix" | "malloc"
+  int process_count;
+  int thread_count;
+  int loop_count;
+  int max_hold_micro_sec;
+  int alloc_size_min;
+  int alloc_size_max;
+};
+
+void child_start(const Parameter& param) {
+  sleep(3);
+}
+
+int main(int argc, char** argv) {
+  if(argc != 8) {
+    std::cerr << "Usage: allocator-test ALLOCATION_METHOD(variable|fix|malloc) PROCESS_COUNT PER_PROCESS_THREAD_COUNT LOOP_COUNT MAX_HOLD_TIME(micro sec) ALLOC_SIZE_MIN ALLOC_SIZE_MAX" << std::endl;
+    return 1;
+  }
+
+  Parameter param = {
+    argv[1],
+    atoi(argv[2]),
+    atoi(argv[3]),
+    atoi(argv[4]),
+    atoi(argv[5]),
+    atoi(argv[6]),
+    atoi(argv[7])
+  };
+
+  if(param.method != "variable" &&
+     param.method != "fix" &&
+     param.method != "malloc") {
+    std::cerr << "Usage: allocator-test ALLOCATION_METHOD(variable|fix|malloc) PROCESS_COUNT PER_PROCESS_THREAD_COUNT LOOP_COUNT MAX_HOLD_TIME(micro sec) ALLOC_SIZE_MIN ALLOC_SIZE_MAX" << std::endl;    
+    return 1;
+  }
+
+  std::vector<pid_t> children(param.process_count);
+  for(int i=0; i < param.process_count; i++) {
+    children[i] = fork();
+    switch(children[i]) {
+    case 0:
+      child_start(param);
+      return 0;
+    case -1:
+      std::cerr << "ERROR: fork() failed: " << strerror(errno) << std::endl;
+      return 1;
+    }
+  }
+  
+  for(int i=0; i < param.process_count; i++) {
+    waitpid(children[i], NULL, 0);
+  }
+
+  return 0;
+}
+
+/*
 typedef imque::allocator::VariableAllocator allocator;
 
 const int CHILD_NUM = 500;
@@ -44,13 +106,6 @@ void child_start(allocator& alc) {
     }
     usleep(rand() % 400); 
     assert(alc.release(idx));
-
-    /*
-    char *buf = new char[size];
-    memset(buf, rand()%0x100, size);
-    usleep(rand() % 400);
-    delete [] buf;
-    */
   }
   std::cout << "# exit: " << getpid() << std::endl;
 }
@@ -86,3 +141,4 @@ int main() {
 
   return 0;
 }
+*/
