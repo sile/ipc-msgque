2b5ca9c30dd30249ef3c015a8e9c3b4441e14503
Takeru Ohta <phjgt308@gmail.com>
2012-09-10 19:19:28 +0900

    enqueueを仮実装

--- include/imque/queue/queue_impl.hh
+++ include/imque/queue/queue_impl.hh
@@ -13,14 +13,12 @@ namespace imque {
 
     // FIFOキュー
     class QueueImpl {
-      struct Entry {
-        uint32_t state:2;
-        uint32_t value:30;
-
-        enum STATE {
-          FREE = 0,
-          USED = 1
-        };
+      struct Node {
+        uint32_t next;
+        uint32_t data_size;
+        char data[0];
+        
+        static const uint32_t END = 0;
       };
 
       struct Stat {
@@ -30,20 +28,16 @@ namespace imque {
       struct Header {
         char magic[sizeof(MAGIC)];
         uint32_t shm_size;
-        uint32_t entry_limit;
 
-        volatile uint32_t version; // tag for ABA problem
-        volatile uint32_t read_pos;
-        volatile uint32_t write_pos;
+        volatile uint32_t head;
+        volatile uint32_t tail;
+        
         Stat stat;
-
-        Entry entries[0];
       };
 
     public:
       QueueImpl(size_t entry_limit, ipc::SharedMemory& shm)
         : shm_size_(shm.size()),
-          entry_limit_(entry_limit),
           que_(shm.ptr<Header>()),
           alc_(shm.ptr<void>(queSize(entry_limit)),
                shm.size() > queSize(entry_limit) ?  shm.size() - queSize(entry_limit) : 0) {
@@ -59,14 +53,16 @@ namespace imque {
       
           memcpy(que_->magic, MAGIC, sizeof(MAGIC));
           que_->shm_size = shm_size_;
-          que_->entry_limit = entry_limit_;
           
-          que_->version = 0;
-          que_->read_pos  = 0;
-          que_->write_pos = 0;
-          que_->stat.overflowed_count = 0;
+          uint32_t md = alc_.allocate(sizeof(Node)); // XXX: 失敗時の処理
+          assert(md != 0);
+          
+          alc_.ptr<Node>(md)->next = Node::END;
+          
+          que_->head = md;
+          que_->tail = md;
 
-          memset(que_->entries, 0, sizeof(Entry)*que_->entry_limit);
+          que_->stat.overflowed_count = 0;
         }
       }
 
@@ -74,8 +70,7 @@ namespace imque {
       // 共有メモリ用のファイルを使い回している場合は、二回目以降は明示的なinit()呼び出しを行った方が安全。
       void init_once() {
         if(*this && (memcmp(que_->magic, MAGIC, sizeof(MAGIC)) != 0 || 
-                     shm_size_ != que_->shm_size ||
-                     entry_limit_ != que_->entry_limit)) {
+                     shm_size_ != que_->shm_size)) {
           init();
         }
       }
@@ -88,24 +83,22 @@ namespace imque {
       // キューに要素を追加する (キューに空きがない場合は false を返す)
       // datav および sizev は count 分のサイズを持ち、それらを全て結合したデータがキューには追加される
       bool enqv(const void** datav, size_t* sizev, size_t count) {
-        if(isFull()) {
-          atomic::add(&que_->stat.overflowed_count, 1);
-          return false;
-        }
-      
         size_t total_size = 0;
         for(size_t i=0; i < count; i++) {
           total_size += sizev[i];
         }
 
-        uint32_t alloc_id = alc_.allocate(sizeof(size_t) + total_size);
+        uint32_t alloc_id = alc_.allocate(sizeof(Node) + total_size);
         if(alloc_id == 0) {
           atomic::add(&que_->stat.overflowed_count, 1);
           return false;
         }
 
-        alc_.ptr<size_t>(alloc_id)[0] = total_size;
-        size_t offset = sizeof(size_t);
+        Node* node = alc_.ptr<Node>(alloc_id);
+        node->next = Node::END;
+        node->data_size = total_size;
+
+        size_t offset = sizeof(Node);
         for(size_t i=0; i < count; i++) {
           memcpy(alc_.ptr<void>(alloc_id, offset), datav[i], sizev[i]);
           offset += sizev[i];
@@ -122,6 +115,8 @@ namespace imque {
 
       // キューから要素を取り出し buf に格納する (キューが空の場合は false を返す)
       bool deq(std::string& buf) {
+        return true;
+        /*
         if(isEmpty()) {
           return false;
         }
@@ -138,51 +133,45 @@ namespace imque {
         assert(alc_.release(alloc_id));
       
         return true;
+        */
       }
 
-      bool isEmpty() const { return que_->read_pos == que_->write_pos; }
-      bool isFull()  const { return que_->read_pos == (que_->write_pos+1) % que_->entry_limit; }
-      size_t entryCount() const { 
-        uint32_t curr_read  = que_->read_pos;
-        uint32_t curr_write = que_->write_pos;
-        if(curr_write >= curr_read) {
-          return curr_write - curr_read;
-        } else {
-          return curr_write + (que_->entry_limit - curr_read);
-        }
-      }
+      bool isEmpty() const { return que_->head == que_->tail; }
 
+      bool isFull()  const { return false; }   // XXX: dummy
+      size_t entryCount() const { return 10; } // XXX: dummy
+      
       // キューへの要素追加に失敗した回数を返す
       size_t overflowedCount() const { return que_->stat.overflowed_count; }
       void resetOverflowedCount() { que_->stat.overflowed_count = 0; }
 
     private:
-      bool enqImpl(uint32_t value) {
-        uint32_t curr_read  = que_->read_pos;
-        uint32_t curr_write = que_->write_pos;
-        uint32_t next_write = (curr_write+1) % que_->entry_limit;
-      
-        if(curr_read == next_write) {
-          atomic::add(&que_->stat.overflowed_count, 1);
-          return false;
-        }
-      
-        Entry* pe = &que_->entries[curr_write];
-        Entry  e = *pe;
-        if(e.state != Entry::FREE) {
-          atomic::compare_and_swap(&que_->write_pos, curr_write, next_write);
-          return enqImpl(value);
-        }
+      bool enqImpl(uint32_t new_tail) {
+        for(;;) {
+          uint32_t tail = que_->tail;
+          if(alc_.refincr(tail) == false) {
+            continue;
+          }
+          
+          Node node = atomic::fetch(alc_.ptr<Node>(tail));
+          if(node.next != Node::END) {
+            atomic::compare_and_swap(&que_->tail, tail, node.next);
+            alc_.release(tail);
+            continue;
+          }
 
-        Entry new_e = {Entry::USED, value};
-        if(atomic::compare_and_swap(pe, e, new_e) == false) {
-          return enqImpl(value);
+          if(atomic::compare_and_swap(&alc_.ptr<Node>(tail)->next, node.next, new_tail)) {
+            atomic::compare_and_swap(&que_->tail, tail, new_tail);
+            alc_.release(tail);
+            return true;
+          }
+          alc_.release(tail);
         }
-      
-        atomic::compare_and_swap(&que_->write_pos, curr_write, next_write);      
+        
         return true;
       }
 
+      /*
       uint32_t deqImpl() {
         uint32_t curr_read  = que_->read_pos;
         uint32_t curr_write = que_->write_pos;
@@ -208,14 +197,14 @@ namespace imque {
         atomic::compare_and_swap(&que_->read_pos, curr_read, next_read);
         return e.value;
       }
+      */
 
       static size_t queSize(size_t entry_limit) {
-        return sizeof(Header) + sizeof(Entry)*entry_limit;
+        return sizeof(Header);
       }
 
     private:
       const uint32_t shm_size_;    // for check
-      const uint32_t entry_limit_; // for check
 
       Header* que_;
       allocator::FixedAllocator alc_;
